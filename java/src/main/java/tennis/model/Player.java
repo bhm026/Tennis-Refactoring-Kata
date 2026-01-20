a43d177c817e856d14506f991964f9adb13a9ff4
bhuvaneshwari <bhuvaneshwari.m.1@maersk.com>
2026-01-20 13:06:07 +0530

    add getter and setters
    Co-authored-by: kartik.porwal@maersk.com

--- java/src/main/java/tennis/model/Player.java
+++ java/src/main/java/tennis/model/Player.java
@@ -3,4 +3,20 @@ package tennis.model;
 public class Player {
     private String name;
     private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
