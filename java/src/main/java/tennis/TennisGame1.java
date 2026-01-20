d1d413133bb4b9e187d8f175d3fc175b2b394d50
Kartik Porwal <kartik.porwal@maersk.com>
2026-01-20 12:46:39 +0530

    Co-authored-by: bhuvaneshwari.m.1@maersk.com
    Refactored the naming for score

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
     private String player1Name;
     private String player2Name;
 
@@ -16,17 +16,17 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (Objects.equals(playerName, "player1"))
-            m_score1 += 1;
+            playerOneScore += 1;
         else
-            m_score2 += 1;
+            playerTwoScore += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (playerOneScore == playerTwoScore)
         {
-            switch (m_score1)
+            switch (playerOneScore)
             {
                 case 0:
                         score = "Love-All";
@@ -43,9 +43,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (playerOneScore >=4 || playerTwoScore >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = playerOneScore - playerTwoScore;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -55,8 +55,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = playerOneScore;
+                else { score+="-"; tempScore = playerTwoScore;}
                 switch(tempScore)
                 {
                     case 0:
