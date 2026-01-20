97f0f8eba85d5a5523f2aed9ddc59cc67464d99b
Kartik Porwal <kartik.porwal@maersk.com>
2026-01-20 12:40:34 +0530

    Co-authored-by: bhuvaneshwari.m.1@maersk.com
    Replaced == operator with .equals method

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -1,5 +1,7 @@
 package tennis;
 
+import java.util.Objects;
+
 public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
@@ -13,7 +15,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (Objects.equals(playerName, "player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
