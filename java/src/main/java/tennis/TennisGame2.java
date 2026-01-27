3c1ac9233f2518ff52b287f5e24cbac461a6f176
bhuvaneshwari <bhuvaneshwari.m.1@maersk.com>
2026-01-27 13:52:22 +0530

    extract method getScoreWhenScoresAreGreaterThanZeroAndScoresAreEqualToZero
    Co-authored-by: kartik.porwal@maersk.com

--- java/src/main/java/tennis/TennisGame2.java
+++ java/src/main/java/tennis/TennisGame2.java
@@ -28,31 +28,8 @@ public class TennisGame2 implements TennisGame
         if (P1point==P2point && P1point>=3) {
             return "tennis.Deuce";
         }
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
+        score = getScoreWhenScoresAreGreaterThanZeroAndScoresAreEqualToZero(P1point, P2point, score);
+
         if (P1point>P2point && P1point < 4)
         {
             if (P1point==2)
@@ -95,6 +72,34 @@ public class TennisGame2 implements TennisGame
         if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
         {
             score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String getScoreWhenScoresAreGreaterThanZeroAndScoresAreEqualToZero(int P1point, int P2point, String score) {
+        if (P1point > 0 && P2point ==0)
+        {
+            if (P1point ==1)
+                P1res = "Fifteen";
+            if (P1point ==2)
+                P1res = "Thirty";
+            if (P1point ==3)
+                P1res = "Forty";
+
+            P2res = "Love";
+            score = P1res + "-" + P2res;
+        }
+        if (P2point > 0 && P1point ==0)
+        {
+            if (P2point ==1)
+                P2res = "Fifteen";
+            if (P2point ==2)
+                P2res = "Thirty";
+            if (P2point ==3)
+                P2res = "Forty";
+
+            P1res = "Love";
+            score = P1res + "-" + P2res;
         }
         return score;
     }
